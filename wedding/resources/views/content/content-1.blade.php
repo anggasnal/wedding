eb5bf3058c0abc6b3ad64bab8e515f78e1447528
angga hasnal akhir <42233373+anggasnal@users.noreply.github.com>
2020-09-19 19:56:22 +0700

    first commit

---
+++ wedding/resources/views/content/content-1.blade.php
@@ -0,0 +1,32 @@
+	<div id="fh5co-couple">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>Hello!</h2>
+					<h3>Juni 27th, 2021 Jakarta Selatan</h3>
+					<p>We invited you to celebrate our wedding</p>
+				</div>
+			</div>
+			<div class="couple-wrap animate-box">
+				<div class="couple-half">
+					<div class="groom">
+						<img src="{{('asset/images/groom.jpg')}}" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-groom">
+						<h3>Angga Hasnal Akhir</h3>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+					</div>
+				</div>
+				<p class="heart text-center"><i class="icon-heart2"></i></p>
+				<div class="couple-half">
+					<div class="bride">
+						<img src="{{('asset/images/bride.jpg')}}" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-bride">
+						<h3>Defi Nurharyanti</h3>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
---
+++ wedding/resources/views/layouts/header.blade.php
@@ -0,0 +1,17 @@
+<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(asset/images/qq.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Angga &amp; Defi </h1><br>
+							<h2>We Are Getting Married</h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							<p><a href="#" class="btn btn-default btn-sm">Save the date</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
